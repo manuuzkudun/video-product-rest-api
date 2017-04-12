94190ae54d5790e9e07423aefda3473a4c3a145f
Manu Uzkudun <manuuzkudun@hotmail.com>
2017-04-12 18:00:13 +0200

    Purchase spec refractored

--- spec/requests/purchases_spec.rb
+++ spec/requests/purchases_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Purchases endpoints for the API', type: :request do
   describe 'POST /purchases' do
 
     context 'when a user purchase a video product' do
-      let!(:purchase) { post api_purchases_path, params: {
+      before(:each) { post api_purchases_path, params: {
                           purchase_option_id: purchase_option.id,
                           video_product_id: video_product.id,
                           user_id: user.id
@@ -24,21 +24,23 @@ RSpec.describe 'Purchases endpoints for the API', type: :request do
     end
 
     context 'when a user tries to buy a video product that is active in his/her library' do
-      let!(:purchase) { FactoryGirl.create(:purchase, user: user, video_product: video_product) }
-      let!(:new_purchase) { post api_purchases_path, params: {
+      before(:each) do
+        FactoryGirl.create(:purchase, user: user, video_product: video_product)
+        post api_purchases_path, params: {
                           purchase_option_id: purchase_option.id,
                           video_product_id: video_product.id,
                           user_id: user.id
                         }
-                      }
+      end
       it 'responds with an HTTP 409 status error' do
         expect(response).to have_http_status(409)
       end
     end
 
     context 'when a user tries to buy a video product that is expired in his/her library' do
-    let!(:purchase) { FactoryGirl.create(:purchase, user: user, video_product: video_product) }
+      let!(:purchase) { FactoryGirl.create(:purchase, user: user, video_product: video_product) }
       before do
+        # Expiration is set automatically before create
         purchase.update(expiration: 2.days.ago)
         post api_purchases_path, params: {
               purchase_option_id: purchase_option.id,
